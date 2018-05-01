7d4da0157f74527606cb229118b3723f5931c35d
Mukesh Soni <mukeshsoni@gmail.com>
2018-05-01 12:22:09 +0530

    added recursive implementation for insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -20,6 +20,34 @@ function insertionSort(arr) {
   return sortedArray
 }
 
+function insertIntoSortedArray(arr, item) {
+  let j = arr.length - 1
+
+  while (item < arr[j] && j >= 0) {
+    arr[j + 1] = arr[j]
+    j--
+  }
+
+  arr[j + 1] = item
+  return arr
+}
+
+/**
+ * recursive implemention for insertion sort
+ * if one element in array, return array
+ * else recursively sort n-1 element array and insert nth element in sorted array
+ */
+function insertionSortRecursive(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  let lastElement = arr[arr.length - 1]
+  let sortedSubArray = insertionSortRecursive(arr.slice(0, arr.length - 1))
+
+  return insertIntoSortedArray(sortedSubArray, lastElement)
+}
+
 /**
  *
  * @param {array} array
@@ -56,15 +84,25 @@ function isSortedDescending(arr) {
   )
 }
 
-let a = range(0, 4)
+let a = range(0, 40)
 a
 let c = shuffle(a)
 c
+let d = shuffle(a)
+d
 console.log(isSortedDescending([]))
 console.log(isSortedDescending([1]))
 console.log(isSortedAscending(a))
 console.log(isSortedDescending(a))
-console.log(insertionSort([4, 1, 2]))
+
+// test for normal loop based insertion sort
+console.log(insertionSort([4, 1, 2, 123, 432, 12, 0, -10]))
 console.log(isSortedAscending(c))
 console.log(isSortedAscending(insertionSort(c)))
 console.log(isSortedDescending(insertionSort(c)))
+
+// test for recursive insertion sort
+console.log(insertionSortRecursive([4, 1, 8, 0, -30, 3, 5, -4, -12]))
+console.log(isSortedAscending(d))
+console.log(isSortedAscending(insertionSortRecursive(d)))
+console.log(isSortedDescending(insertionSortRecursive(d)))
